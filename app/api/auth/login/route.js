2626c8f83e5ed90273c18e6bccda190e3b770ef3
shubh mishra <smishra64310@gmail.com>
2026-06-21 16:58:31 +0530

    completed and tested the login api

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,8 +1,63 @@
+import { generateToken, verifyPassword } from "@/lib/auth";
 import prisma from "@/lib/prisma";
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export  async function POST(request){
-    const {email,password}= await request.json()
+export async function POST(request) {
+  try {
+    const { email, password } = await request.json();
 
-    
+    if (!email || !password) {
+      return NextResponse.json(
+        {
+          error: "Email & Password is required or not valid",
+        },
+        { status: 400 },
+      );
+    }
+
+    const userFromDb = await prisma.user.findUnique({ where: { email } });
+
+    if (!userFromDb) {
+      return NextResponse.json(
+        { message: "invalid user not found" },
+        { status: 401 },
+      );
+    }
+    const isValidPassword = await verifyPassword(password, userFromDb.password);
+
+    if (!isValidPassword) {
+      return NextResponse.json(
+        { message: "invalid credentials" },
+        { status: 401 },
+      );
+    }
+
+    const token = generateToken(userFromDb.id);
+
+    const response = NextResponse.json({
+      user: {
+        id: userFromDb.id,
+        name: userFromDb.name,
+        email: userFromDb.email,
+        image: userFromDb.image,
+        emailVerified: userFromDb.emailVerified,
+        token,
+      },
+    });
+
+    response.cookies.set("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      maxAge: 60 * 60 * 24 * 7,
+    });
+
+    return response;
+  } catch (error) {
+    console.error("login failed", error);
+    return NextResponse.json(
+      { error: "internal server error" },
+      { status: 500 },
+    );
+  }
 }
--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -6,11 +6,75 @@ import { registerSchema } from "@/lib/validations";
 import { z } from "zod";
 import prisma from "@/lib/prisma";
 
+// export async function POST(request) {
+//   try {
+//     const body = await request.json();
+
+//     // Step 1: Zod se validate karo
+//     const result = registerSchema.safeParse(body);
+//     if (!result.success) {
+//       return NextResponse.json(
+//         {
+//           error: "Validation failed",
+//           details: z.flattenError(result.error),
+//         },
+//         { status: 400 },
+//       );
+//     }
+
+//     const { name, email, password, phone } = result.data;
+
+//     // Step 2: Check karo email already registered hai ya nahi
+//     const existingUser = await prisma.user.findUnique({ where: { email } });
+//     if (existingUser) {
+//       return NextResponse.json(
+//         { error: "Email already registered" },
+//         { status: 409 },
+//       );
+//     }
+
+//     // Step 3: Password hash karo aur user create karo
+
+//     const hashedPassword = await hashPassword(password);
+
+//     const user = await prisma.user.create({
+//       data: {
+//         name,
+//         email,
+//         password: hashedPassword,
+//         phone: phone === "" ? null : phone,
+//       },
+//     });
+
+//     // Step 4: Token generate karke cookie mein set karo
+//     const token = await generateToken(user);
+//     const cookieStore = await cookies();
+//     cookieStore.set("token", token, {
+//       httpOnly: true,
+//       secure: process.env.NODE_ENV === "production",
+//       sameSite: "lax",
+//       maxAge: 60 * 60 * 24 * 7,
+//       path: "/",
+//     });
+
+//     // Step 5: Response bhejo (password kabhi mat bhejo!)
+//     return NextResponse.json(
+//       { id: user.id, name: user.name, email: user.email },
+//       { status: 201 },
+//     );
+//   } catch (error) {
+//     console.error("Register Error:", error);
+//     return NextResponse.json(
+//       { error: "Something went wrong" },
+//       { status: 500 },
+//     );
+//   }
+// }
+
 export async function POST(request) {
   try {
     const body = await request.json();
 
-    // Step 1: Zod se validate karo
     const result = registerSchema.safeParse(body);
     if (!result.success) {
       return NextResponse.json(
@@ -24,7 +88,6 @@ export async function POST(request) {
 
     const { name, email, password, phone } = result.data;
 
-    // Step 2: Check karo email already registered hai ya nahi
     const existingUser = await prisma.user.findUnique({ where: { email } });
     if (existingUser) {
       return NextResponse.json(
@@ -32,23 +95,22 @@ export async function POST(request) {
         { status: 409 },
       );
     }
-
-    // Step 3: Password hash karo aur user create karo
-
     const hashedPassword = await hashPassword(password);
-
     const user = await prisma.user.create({
       data: {
         name,
         email,
         password: hashedPassword,
         phone: phone === "" ? null : phone,
+        image: null,
+        emailVerified: null,
       },
     });
 
-    // Step 4: Token generate karke cookie mein set karo
     const token = await generateToken(user);
+
     const cookieStore = await cookies();
+
     cookieStore.set("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
@@ -57,15 +119,21 @@ export async function POST(request) {
       path: "/",
     });
 
-    // Step 5: Response bhejo (password kabhi mat bhejo!)
     return NextResponse.json(
-      { id: user.id, name: user.name, email: user.email },
+      {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+      
       { status: 201 },
     );
   } catch (error) {
-    console.error("Register Error:", error);
+    console.error("registrtion failed");
     return NextResponse.json(
-      { error: "Something went wrong" },
+      {
+        error: "Somthing went wrong",
+      },
       { status: 500 },
     );
   }
